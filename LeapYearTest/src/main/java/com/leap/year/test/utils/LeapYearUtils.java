09acb19f6a80b655da2b01aa1fe6e6490e0c187a
Mukesh Mohan <mukeshvrm49@gmail.com>
2020-05-16 20:24:46 +0530

    Initial check in for leap year test cases

---
+++ LeapYearTest/src/main/java/com/leap/year/test/utils/LeapYearUtils.java
@@ -0,0 +1,57 @@
+package com.leap.year.test.utils;
+
+/**
+ * 
+ * Utility class which provide the methods to determine the given year is a leap year or not
+ *
+ */
+public class LeapYearUtils {
+
+	/**
+	 * This method check that given year is divisible by 400 or not 
+	 * 
+	 * @param year
+	 * @return true/false
+	 */
+	public static boolean isDivisibleBy400(final int year){
+		
+		return (year % 400 == 0);
+	
+	}
+	
+	/**
+	 * This method check that given year is divisible by 100 but not by 400 
+	 * 
+	 * @param year
+	 * @return true/false
+	 */
+	public static boolean isDivisibleBy100NotBy400(final int year){
+		
+		return (year % 100 == 0) == (year % 400 == 0);
+	
+	}
+	
+	/**
+	 * This method check that given year is divisible by 4 but not by 100 
+	 * 
+	 * @param year
+	 * @return true/false
+	 */
+	public static boolean isDivisibleBy4NotBy100(final int year){
+		
+		return (year % 4 == 0) != (year % 100 == 0);
+	
+	}
+	
+	/**
+	 * This method check that given year is divisible by 4
+	 * 
+	 * @param year
+	 * @return true/false
+	 */
+	public static boolean isDivisibleBy4(final int year){
+		
+		return (year % 4 == 0);
+	
+	}
+}
---
+++ LeapYearTest/src/test/java/com/leap/year/test/cases/LeapYearTestCases.java
@@ -0,0 +1,56 @@
+package com.leap.year.test.cases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.leap.year.test.utils.LeapYearUtils;
+
+/**
+ * 
+ * Unit test cases to determine given year is a leap year or not.
+ *
+ */
+public class LeapYearTestCases {
+
+	/**
+	 * Test case 1 to test the given year is a leap year if divisible by 400.
+	 */
+	@Test
+	public void divideBy400Test() {
+		String message = "Leap year must be divide by 400";
+		boolean status = LeapYearUtils.isDivisibleBy400(2000);
+		assertEquals(message,  true, status);
+	}
+
+	/**
+	 * Test case 2 to test the given year is not a leap year if not divisible by 100 and 400.
+	 */
+	@Test
+	public void divideBy100NotBy400Test() {
+		String message = "Leap year must be divide by 100 and 400";
+		boolean status = LeapYearUtils.isDivisibleBy100NotBy400(2000);
+		assertEquals(message,  true, status);
+	}
+	
+	/**
+	 * Test case 3 to test the given year is a leap year if divisible by 4 and not by 100.
+	 */
+	@Test
+	public void divideBy4NotBy100Test() {
+		String message = "Leap year must be divide by 4 and not divide by 100";
+		boolean status = LeapYearUtils.isDivisibleBy4NotBy100(2016);
+		assertEquals(message,  true, status);
+	}
+	
+	/**
+	 * Test case 4 to test the given year is a leap year if divisible by 4.
+	 */
+	@Test
+	public void divideBy4Test() {
+		String message = "Leap year must be divide by 4";
+		boolean status = LeapYearUtils.isDivisibleBy4(2016);
+		assertEquals(message,  true, status);
+	}
+	
+}
